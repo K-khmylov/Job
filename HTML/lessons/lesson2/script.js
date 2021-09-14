4a79f449f35a6ba02248ab2e4913c3072b3f0f55
NotKostya <36542563@bk.ru>
2021-09-14 19:50:50 +0300

    commit!

--- HTML/lessons/lesson2/script.js
+++ HTML/lessons/lesson2/script.js
@@ -1,4 +1,4 @@
 let pr=document.getElementsByTagName("p");
 let elem=pr[pr.length-1];
-elem.onclick=function(){alert('Позлравляю, все сделали правильно ))))')};
+elem.onclick=function(){alert('Поздравляю, все сделали правильно!')};
 
